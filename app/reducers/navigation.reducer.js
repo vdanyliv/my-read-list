8f22c1a749f689b5e79c4b3685a2a554a206737e
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-10-07 16:41:18 +0300

    new navigation reducer

---
+++ app/reducers/navigation.reducer.js
@@ -0,0 +1,19 @@
+export default function() {
+	return [
+		{
+			id: 1,
+			link: '#/home',
+			name: 'Home'
+		},
+		{
+			id: 2,
+			link: '#/about',
+			name: 'About'
+		},
+		{
+			id: 3,
+			link: '#/version',
+			name: 'Version'
+		}
+	]
+}
