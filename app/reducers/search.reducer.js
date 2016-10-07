2c11599f9a8e58ff078c4c5358a5d2499f2ebfb4
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-10-07 16:41:04 +0300

    Search reducer

---
+++ app/reducers/search.reducer.js
@@ -0,0 +1,19 @@
+export default function() {
+	return [
+		{
+			id: 1,
+			author: 'Jon Travor',
+			book: 'The lost island'
+		},
+		{
+			id: 2,
+			author: 'Bob Biligan',
+			book: 'How i kill your cat'
+		},
+		{
+			id: 3,
+			author: 'Josh Copini',
+			book: 'We found body on the river'
+		}
+	]
+}
