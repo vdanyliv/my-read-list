9ad3aeb7c947b50582c23d371ed315b706c2f7b2
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-10-07 16:43:36 +0300

    Added new container into app component

---
+++ app/components/books/app.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Navigation from 'containers/navigation';
+import Search from 'containers/search';
+import Books from 'containers/books';
+
+require('common/style/css/bootstrap.css');
+require('common/style/css/custom.css');
+
+class App extends Component {
+	render() {
+		return (
+				<div className="row main-container">
+					<div className="row nav-container">
+						<Navigation />
+					</div>
+					<div className="row col-md-12">
+						<Search />
+					</div>
+					<div className="row col-md-12">
+						<Books />
+					</div>
+					<div className="row col-md-12 footer-container">
+						Footer
+					</div>
+				</div>
+		);
+	}
+}
+
+export default App;
