163da6083d1e0d4d50fd082c010a99f677f0a2c1
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-10-28 12:23:55 +0300

    Added bookSearch action

--- app/actions/book.select.js
+++ app/actions/book.select.js
@@ -1,6 +1,18 @@
+/*ACTION TYPE*/
+export const BOOK_SELECT = 'BOOK_SELECT';
+export const BOOK_SEARCH = 'BOOK_SEARCH';
+
+/*ACTION CREATORS*/
 export const selectBook =  (bookItem) => {
 	return {
-		type: 'BOOK_SELECT',
+		type: BOOK_SELECT,
 		payload: bookItem
 	}
 } 
+
+export const bookSearch = (name) => {
+	return {
+		type: BOOK_SEARCH,
+		name
+	}
+}
