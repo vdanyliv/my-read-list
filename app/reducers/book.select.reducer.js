6147f88e466e1cf10eefcb38e502432c28e8bad4
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-10-28 18:45:09 +0300

    Make separate reducer for searching and selecting

--- app/reducers/book.select.reducer.js
+++ app/reducers/book.select.reducer.js
@@ -1,16 +1,9 @@
 export default function(state = [], action) {
-	const initialState = {
-		books: [],
-		search: []
-	}
-
 	switch (action.type) {
 		case 'BOOK_SELECT':
 			return Object.assign({}, state, {
 				selectedBook: action.payload
 			});
-		case 'BOOK_SEARCH':
-			console.error(state, action, 'BOOK_SEARCH');
 		default:
 			return state;
 	}
