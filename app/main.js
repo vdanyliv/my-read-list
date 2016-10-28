6610523c561c7e5221c353cca6dc537facefb20f
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-10-28 12:23:29 +0300

    enabled redux devtools

--- app/main.js
+++ app/main.js
@@ -5,11 +5,11 @@ import { createStore } from 'redux';
 import allReducers from "./reducers";
 import App from './components/books/app';
 
-const store = createStore(allReducers);
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 let rootElement =  document.getElementById('root');
 
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
-	</Provider>,
+	</Provider>, 
 rootElement);
