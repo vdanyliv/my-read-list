d2d200f7d5fedf61d7480cd164103ce109ca0334
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-10-30 14:16:15 +0200

    Removed search result for "input container"

--- app/containers/search.js
+++ app/containers/search.js
@@ -4,26 +4,12 @@ import { connect } from 'react-redux';
 import { bookSearch } from 'actions/book.select';
 
 class Search extends Component {
-	showSelected() {
-		if (this.props.founded.length) {
-			return this.props.founded.map((item) => {
-				return(
-					<div key={ item.id }>
-						<div>{ item.author }</div>
-					</div>
-				)
-			})
-		}
-	}
-
 	render() {
 		return (
 			<div className="col-md-12 search-container">
 				<input onInput={(e) => {
 						bookSearch(this.props.bookSearch({ value: e.currentTarget.value, books: this.props.books}))
 					}} type="text" className="form-control" placeholder="Search book!" />
-
-				<div>you typed: { this.showSelected() }</div>
 			</div>
 		)
 	}
@@ -31,8 +17,7 @@ class Search extends Component {
 
 function mapStateToProps(state) {
 	return {
-		books: state.books,
-		founded: state.bookSearch
+		books: state.books
 	}
 }
 
