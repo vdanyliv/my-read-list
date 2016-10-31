57f47db3b093d2bf5d099a657dbf3cebdbffec4a
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-10-31 18:01:13 +0200

    added redux-thunk

--- app/main.js
+++ app/main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import allReducers from "./reducers";
 import App from './components/books/app';
 
-const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = applyMiddleware(thunk)(createStore)(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 let rootElement =  document.getElementById('root');
 
 ReactDOM.render(
