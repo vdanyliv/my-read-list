39116ccaa717414bb6cf96dae9d9cae6736bf32e
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-10-31 18:00:36 +0200

    Implemented new actions for search, used redux-thunk for async actions

---
+++ app/actions/books.search.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+export const FETCH_BOOKS_REQUEST = 'FETCH_BOOKS_REQUEST';
+export const FETCH_BOOKS_FAILURE = 'FETCH_BOOKS_FAILURE';
+export const FETCH_BOOKS_SUCCESS = 'FETCH_BOOKS_SUCCESS';
+
+export const booksRequest = () => {
+	return {
+		type: FETCH_BOOKS_REQUEST
+	}
+}
+
+export const booksRequestFailure = () => {
+	return {
+		type: FETCH_BOOKS_FAILURE
+	}
+}
+
+export const booksRequestSuccess = (items) => {
+	return {
+		type: FETCH_BOOKS_SUCCESS,
+		items
+	}
+}
+
+export const requestSearch = (keyword) => {
+  return dispatch => {
+  	dispatch(booksRequest());
+		axios.get('https://www.googleapis.com/books/v1/volumes?q=' + keyword)
+			.then((response) => {
+				if (response.data.items.length) {
+					dispatch(booksRequestSuccess(response.data.items));
+				}
+			})
+			.catch(() => {
+				dispatch(booksRequestFailure())
+			})
+  };
+}
