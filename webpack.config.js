cffc8cd2f10675592de2e0d65332be2aa2b309dd
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-01 17:13:16 +0200

    added webpack-image-loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,12 +21,24 @@ module.exports = {
             {
                 test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
                 loader : 'file-loader'
+            },
+            {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
             }
         ]
     },
     plugins: [
         new CopyWebpackPlugin([
-            { from: "./app/index.html", to: "index.html" }
+            { 
+                from: "./app/index.html", to: "index.html",                
+            },
+            { 
+                from: "./app/common/img", to: "img/",                
+            }
         ])
     ],
     resolve: {
