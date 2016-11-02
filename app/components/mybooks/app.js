6f867ccc0ccd87d75e89963740a21684b134149c
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-02 14:33:08 +0200

    New component "My books"

---
+++ app/components/mybooks/app.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Navigation from 'containers/common/navigation';
+
+require('common/style/css/bootstrap.css');
+require('common/style/css/custom.css');
+
+class App extends Component {
+	render() {
+		return (
+				<div className="row main-container">
+					<div className="row nav-container">
+						<Navigation />
+					</div>
+					<div className="row col-md-12">
+						Some info
+					</div>
+				</div>
+		);
+	}
+}
+
+export default App;
