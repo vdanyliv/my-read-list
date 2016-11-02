22295998e85e61b9154558c3ad73bab87a4cd389
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-02 14:34:33 +0200

    Added react-route

--- app/main.js
+++ app/main.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
+import { Router, Route, Link, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import allReducers from "./reducers";
-import App from './components/books/app';
+import Main from './components/books/app';
+import MyBooks from './components/mybooks/app';
 
 const store = applyMiddleware(thunk)(createStore)(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 let rootElement =  document.getElementById('root');
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<Router history={browserHistory}>
+		    <Route path="/" component={Main}></Route>
+		    <Route path="mybooks" component={MyBooks}></Route>
+		</Router>
 	</Provider>, 
 rootElement);
