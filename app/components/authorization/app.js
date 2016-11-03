b3caefde0ce3684e17f8b98d9ad9ae4ae37fa7c5
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-03 17:18:20 +0200

    New authorization component and signin container

---
+++ app/components/authorization/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Navigation from 'containers/common/navigation';
+import SignIn from 'containers/authorization/signin';
+
+class App extends Component {
+	render() {
+		return (
+				<div className="row main-container">
+					<div className="row nav-container">
+						<Navigation />
+					</div>
+					<div className="row col-md-12">
+						<SignIn />
+					</div>
+				</div>
+		);
+	}
+}
+
+export default App;
