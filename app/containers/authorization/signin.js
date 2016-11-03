1643cb513f577b9ab8310c735232f9b0b962afeb
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-03 17:20:46 +0200

    New signin container

---
+++ app/containers/authorization/signin.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+class SignIn extends Component {
+	render() {
+		return (
+			<div>
+				<p>My Form</p>
+			</div>
+		)
+	}
+}
+
+export default SignIn;
