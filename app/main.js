44949db707a62ad9947bedb6bbff2fa799886f26
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-03 17:19:18 +0200

    Added new route "signin"

--- app/main.js
+++ app/main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
-import { Router, Route, Link, browserHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import allReducers from "./reducers";
 import Main from './components/books/app';
 import MyBooks from './components/mybooks/app';
+import Auth from './components/authorization/app';
 
 const store = applyMiddleware(thunk)(createStore)(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 let rootElement =  document.getElementById('root');
@@ -16,6 +17,7 @@ ReactDOM.render(
 		<Router history={browserHistory}>
 		    <Route path="/" component={Main}></Route>
 		    <Route path="mybooks" component={MyBooks}></Route>
+		    <Route path="signin" component={Auth}></Route>
 		</Router>
 	</Provider>, 
 rootElement);
