4b07431006c343efea80eb631f13d521f03b4a4c
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-08 11:44:24 +0200

    SignIn page | login/reg containers

---
+++ app/containers/authorization/forms/login.form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+	render() {
+		return (
+			<div>
+				Login form!
+			</div>
+		)
+	}
+}
+
+export default LoginForm;
--- app/containers/authorization/signin.js
+++ app/containers/authorization/signin.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import LoginForm from './forms/login.form';
+import RegForm from './forms/reg.form'
 
 class SignIn extends Component {
-	render() {
-		return (
-			<div>
-				<p>My Form</p>
-			</div>
-		)
-	}
+  constructor() {
+    super();
+    this.state = {
+      activeState: null
+    }
+  }
+  
+  showAuthForm(type) {
+    this.setState({ activeState: type });
+  }
+
+  render() {
+    return (
+      <div className="row col-md-12">
+        <h3>Authorization</h3>
+        <div className="col-md-6">
+          <div className="col-md-2">
+           <button onClick={() => this.showAuthForm('reg') }>Regirtration</button>
+          </div>
+          <div className="col-md-2">
+           <button onClick={() => this.showAuthForm('login') }>Login</button>
+          </div>
+        </div>
+        <div className="col-md-6">
+          { this.state.activeState === 'reg' && <RegForm /> }
+          { this.state.activeState === 'login' && <LoginForm /> }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default SignIn;
