c8568d9ef07a0d11a174c90653b97c7b9947f6f9
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-09 14:26:34 +0200

    Changes in code style

--- app/containers/books/search.js
+++ app/containers/books/search.js
@@ -4,40 +4,38 @@ import { connect } from 'react-redux';
 import { requestSearch } from 'actions/books.search';
 
 class Search extends Component {
-	constructor() {
-		super();
-		this.searchDebounce;
-	}
-	searchInDirectory(keyword) {
-		if (keyword.length > 2) {
-				clearTimeout(this.searchDebounce);
-				this.searchDebounce = setTimeout(() => {
-					requestSearch(this.props.requestSearch(keyword));
-				}, 800);
-		}
-	}
-
-	render() {
-		return (
-			<div className="col-md-12 search-container">
-				<input onInput={(e) => {
-						this.searchInDirectory(e.currentTarget.value);
-					}} type="text" className="form-control" placeholder="Search book!" />
-			</div>
-		)
-	}
+  constructor() {
+    super();
+    this.searchDebounce = null;
+  }
+  searchInDirectory(keyword) {
+    if (keyword.length > 2) {
+      clearTimeout(this.searchDebounce);
+      this.searchDebounce = setTimeout(() => {
+        requestSearch(this.props.requestSearch(keyword));
+      }, 800);
+    }
+  }
+  render() {
+    return (
+      <div className="col-md-12 search-container">
+        <input onInput={e => this.searchInDirectory(e.currentTarget.value)}
+          type="text"
+          className="form-control"
+          placeholder="Search book!" />
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		books: state.books
-	}
+  return {
+    books: state.books
+  };
 }
 
-function matchDispatchToProps(dispatch) {
-	return bindActionCreators({ 
-		requestSearch: requestSearch
-	}, dispatch);
+function mapDispatchToProps(dispatch) {
+  return { requestSearch: bindActionCreators(requestSearch, dispatch) };
 }
 
-export default connect(mapStateToProps, matchDispatchToProps)(Search);
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
