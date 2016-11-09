7edf749d3489e02a835b52b94249aff97ef73586
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-09 10:35:52 +0200

    user schema for db

---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+let mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+  local            : {
+    email        : String,
+    password     : String
+  },
+  facebook         : {
+    id           : String,
+    token        : String,
+    email        : String,
+    name         : String
+  },
+  twitter          : {
+    id           : String,
+    token        : String,
+    displayName  : String,
+    username     : String
+  },
+  google           : {
+    id           : String,
+    token        : String,
+    email        : String,
+    name         : String
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
