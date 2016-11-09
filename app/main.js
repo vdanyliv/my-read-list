7a2cf9cb5f3924cdd1be1bb8ca27a267e8fa768b
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-09 14:08:44 +0200

    Changes in code style

--- app/main.js
+++ app/main.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
-import { Router, Route, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import allReducers from "./reducers";
+import {Router, Route, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import allReducers from './reducers';
 import Main from './components/books/app';
 import MyBooks from './components/mybooks/app';
 import Auth from './components/authorization/app';
 
 const store = applyMiddleware(thunk)(createStore)(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-let rootElement =  document.getElementById('root');
+const rootElement = document.getElementById('root');
 
 ReactDOM.render(
 	<Provider store={store}>
 		<Router history={browserHistory}>
-		    <Route path="/" component={Main}></Route>
-		    <Route path="mybooks" component={MyBooks}></Route>
-		    <Route path="signin" component={Auth}></Route>
+      <Route path="/" component={Main}></Route>
+      <Route path="mybooks" component={MyBooks}></Route>
+      <Route path="signin" component={Auth}></Route>
 		</Router>
-	</Provider>, 
+	</Provider>,
 rootElement);
