31a913d03a707cab10ddd6562268a8fb568eff35
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-14 17:48:07 +0200

    CChanges in code style

--- app/actions/book.select.js
+++ app/actions/book.select.js
@@ -1,9 +1,9 @@
 export const BOOK_SELECT = 'BOOK_SELECT';
 export const BOOK_SEARCH = 'BOOK_SEARCH';
 
-export const selectBook = (bookItem) => {
-	return {
-		type: BOOK_SELECT,
-		payload: bookItem
-	};
+export const selectBook = bookItem => {
+  return {
+    type: BOOK_SELECT,
+    payload: bookItem
+  };
 };
