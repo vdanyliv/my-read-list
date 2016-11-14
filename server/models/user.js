a766a6cf5ada7ddaa0263dd5709ebeae1c7db135
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-14 17:36:16 +0200

    User model, minor changes

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var userSchema = mongoose.Schema({
+module.exports = mongoose.model('User', new Schema({
   local: {
     name: String,
     password: String
   }
-});
-
-module.exports = mongoose.model('User', userSchema);
+}));
