2999a10a69c74f2ae7e207155964c2d60453ac81
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-14 17:47:49 +0200

    Created action for signup and signin

--- app/actions/user.js
+++ app/actions/user.js
@@ -1,38 +1,70 @@
 import axios from 'axios';
 
-export const AUTHORIZE_SUCCESS = 'AUTHORIZE_SUCCESS';
-export const REGISTRATION_REQUEST = 'REGISTRATION_REQUEST';
-export const REGISTRATION_SUCCESS = 'REGISTRATION_SUCCESS';
-export const REGISTRATION_FAILURE = 'REGISTRATION_FAIL';
+export const SIGNIN_SUCCESS = 'SIGNIN_SUCCESS';
+export const SIGNIN_FAILURE = 'SIGNIN_FAILURE';
+export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'SIGNUP_FAIL';
 
 export const registrationRequest = () => {
   return {
-    type: REGISTRATION_REQUEST
+    type: SIGNUP_REQUEST
   };
 };
 
 export const registrationSuccess = authResult => {
   return {
-    type: REGISTRATION_SUCCESS,
+    type: SIGNUP_SUCCESS,
     auth: authResult
   };
 };
 
 export const registrationFailure = () => {
   return {
-    type: REGISTRATION_FAILURE
+    type: SIGNUP_FAILURE
   };
 };
 
-export const registerUser = formData => {
+export const authFailure = authResult => {
+  return {
+    type: SIGNIN_FAILURE,
+    auth: authResult
+  };
+};
+
+export const authSuccess = authResult => {
+  return {
+    type: SIGNIN_SUCCESS,
+    auth: authResult
+  };
+};
+
+export const signupUser = formData => {
   return dispatch => {
     dispatch(registrationRequest());
-    axios.post('http://localhost:9001/registration', formData)
+    axios.post('http://localhost:9001/signup', formData)
     .then(result => {
+      document.cookie = 'token=' + result.data.secretToken;
       dispatch(registrationSuccess(result));
     })
     .catch(() => {
       dispatch(registrationFailure);
     });
+  };
+};
+
+export const signinUser = formData => {
+  return dispatch => {
+    axios.post('http://localhost:9001/signin', formData)
+      .then(result => {
+        if (result.data.error) {
+          dispatch(authFailure(result.data));
+        } else {
+          dispatch(authSuccess(result.data));
+        }
+      })
+      .catch(err => {
+        dispatch(authFailure(err.data));
+      });
   };
 };
