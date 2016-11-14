9fba287c8bdf9fcaa17bd192355c3c58e1034250
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-14 17:38:34 +0200

    Implementation signup, signin API with JWT

---
+++ server/api/auth-api.js
@@ -0,0 +1,60 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+module.exports = app => {
+  app.post('/signup', (req, res) => {
+    const userName = req.body.name;
+    const userPassword = req.body.password;
+
+    User.findOne({ 'local.name': userName }, (err, user) => {
+      if (err) throw err;
+      if (user) {
+        res.json({
+          error: 'Username already used!'
+        });
+      } else {
+        const newUser = new User();
+
+        newUser.local.name = userName;
+        newUser.local.password = userPassword;
+
+        newUser.save(saveError => {
+          if (saveError) throw saveError;
+          const token = jwt.sign(newUser, app.get('secret'), {
+            expiresIn: 1440
+          });
+
+          res.json({
+            secretToken: token
+          });
+        });
+      }
+    });
+  });
+
+  app.post('/signin', (req, res) => {
+    const userName = req.body.name;
+    const userPassword = req.body.password;
+
+    User.findOne({ 'local.name': userName }, (err, user) => {
+      if (!user) {
+        res.json({ 'error': 'User not found!' });
+      } else if (user.local) {
+        if (user.local.password !== userPassword) {
+          res.json({ 'error': 'Password wrong' });
+        } else {
+          const token = jwt.sign(user.local, app.get('secret'), {
+            expiresIn: 1440
+          });
+
+          res.json({
+            user: user.local.name,
+            authorized: true,
+            secretToken: token
+          });
+        }
+      }
+    });
+  });
+};
+
