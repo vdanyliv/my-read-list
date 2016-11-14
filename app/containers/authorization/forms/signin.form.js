f7f12f7e41fbc659066a66ae1656cfccf3545690
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-14 17:48:47 +0200

    Implemented SingUp/SignIn form

---
+++ app/containers/authorization/forms/signin.form.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { signinUser } from 'actions/user';
+
+class LoginForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      password: '',
+      token: ''
+    };
+  }
+
+  authorizeUser(e) {
+    signinUser(this.props.authUser(this.state));
+    e.preventDefault();
+  }
+
+  nameChange(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  passwordChange(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={e => this.authorizeUser(e)}>
+        <div className="row">
+          { this.props.user.error && <div className="alert alert-danger">{this.props.user.error}</div> }
+        </div>
+        <div>
+          <h3>Login</h3>
+          <div className="form-group">
+          <label htmlFor="name">Your nickname:</label>
+          <input type="text" className="form-control" classID="name" required value={this.state.name} onChange={e => this.nameChange(e)} />
+          </div>
+          <div className="form-group">
+          <label htmlFor="password">Your password</label>
+          <input type="password" className="form-control" classID="password" required value={this.state.password} onChange={e => this.passwordChange(e)} />
+          </div>
+          <div className="form-group">
+          <button className="btn btn-default" role="button" type="submit">Login</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    authUser: bindActionCreators(signinUser, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
---
+++ app/containers/authorization/forms/signup.form.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { signupUser } from 'actions/user';
+
+class RegForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      password: ''
+    };
+  }
+  createNewUser(event) {
+    signupUser(this.props.newUser(this.state));
+    event.preventDefault();
+  }
+  nameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+  passwordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+  render() {
+    return (
+      <form onSubmit={e => this.createNewUser(e)}>
+        <div>
+          <h3>Registration</h3>
+          <div className="form-group">
+          <label htmlFor="name">Your nickname:</label>
+          <input type="text" className="form-control" classID="name" required value={this.state.name} onChange={e => this.nameChange(e)} />
+          </div>
+          <div className="form-group">
+          <label htmlFor="password">Your password</label>
+          <input type="password" className="form-control" classID="password" required value={this.state.password} onChange={e => this.passwordChange(e)} />
+          </div>
+          <div className="form-group">
+          <button className="btn btn-default" role="button" type="submit">Register</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    newUser: bindActionCreators(signupUser, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegForm);
