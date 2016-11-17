c910f4b60c180df0bd9d8bcc98a53a1791c5f81d
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-17 18:50:48 +0200

    Added user roles for each navigation item

--- app/containers/common/navigation.js
+++ app/containers/common/navigation.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 class Navigation extends Component {
-	createListItem() {
+  createListItem() {
+    return this.props.nav.map(item => {
+      const showElem = item.userGroup.indexOf(this.props.user.group);
 
-		return this.props.nav.map((item) => {
-			return (
-				<li key={ item.id }><Link to={ item.link }>{item.name}</Link></li>
-			)
-		});
-	}
+      if (showElem === -1) return false;
 
-	render() {
-		return (
-			<ul className="nav nav-pills">
-				{ this.createListItem() }
-			</ul>
-		)
-	}
+      return (
+        <li key={ item.id }>
+          <Link to={ item.link }>{item.name}</Link>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="nav nav-pills">
+        { this.createListItem() }
+      </ul>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		nav: state.navigation
-	}
+  return {
+    nav: state.navigation,
+    user: state.user
+  };
 }
 
 export default connect(mapStateToProps)(Navigation);
--- app/reducers/navigation.reducer.js
+++ app/reducers/navigation.reducer.js
@@ -1,24 +1,28 @@
 export default function() {
-	return [
-		{
-			id: 1,
-			link: '/',
-			name: 'Home'
-		},
-		{
-			id: 2,
-			link: 'mybooks',
-			name: 'My books'
-		},
-		{
-			id: 3,
-			link: 'signin',
-			name: 'Signin'
-		},
-		{
-			id: 4,
-			link: 'version',
-			name: 'Version'
-		}
-	]
+  return [
+    {
+      id: 1,
+      link: '/',
+      name: 'Home',
+      userGroup: ['member', 'guest']
+    },
+    {
+      id: 2,
+      link: '/mybooks',
+      name: 'My books',
+      userGroup: ['member']
+    },
+    {
+      id: 3,
+      link: '/signin',
+      name: 'Signin',
+      userGroup: ['guest']
+    },
+    {
+      id: 4,
+      link: '/version',
+      name: 'Version',
+      userGroup: ['member', 'guest']
+    }
+  ];
 }
