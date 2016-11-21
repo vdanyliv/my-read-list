759298860c43cee562b28c0f382dd644d42b8312
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-21 17:08:14 +0200

    Minor changes

--- index.js
+++ index.js
@@ -46,8 +46,7 @@ if (env === 'dev') {
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   });
