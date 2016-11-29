9a8b48d698ab90a24796c0564877bf345cfa9f7a
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-29 20:06:26 +0200

    Checking if user authorized before rendering application

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import {Router, Route, browserHistory} from 'react-router';
@@ -8,17 +8,24 @@ import allReducers from './reducers';
 import Main from './components/books/app';
 import MyBooks from './components/mybooks/app';
 import Auth from './components/authorization/app';
-import authorizationService from './services/authorization-service';
+import loginRequired from './services/route-auth';
 
 const store = applyMiddleware(thunk)(createStore)(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(
-    <Provider store={store}>
-        <Router history={browserHistory}>
-          <Route path="/" component={Main}></Route>
-          <Route path="mybooks" component={MyBooks} onEnter={authorizationService.checkAuth}></Route>
-          <Route path="signin" component={Auth}></Route>
-        </Router>
-    </Provider>,
-rootElement);
+class App extends Component {
+  componentWillMount() {
+    loginRequired(store.dispatch);
+  }
+  render() {
+    return (
+      <Router history={ browserHistory }>
+        <Route path="/" component={Main}></Route>
+        <Route path="mybooks" component={MyBooks}></Route>
+        <Route path="signin" component={Auth}></Route>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
