1bdc09d03e9435019a057aeca26a55df21f0ac0a
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-29 20:09:34 +0200

    Added callback for dispatching auth event

--- app/services/route-auth.js
+++ app/services/route-auth.js
@@ -1,32 +1,35 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
-module.exports = (nextState, replace, next) => {
+module.exports = callback => {
   const documentCookies = document.cookie;
   const splitingCookies = documentCookies.split(';');
   let secret = '';
 
-  console.error(arguments);
-
   for (let i = 0; i < splitingCookies.length; i++) {
     if (splitingCookies[i].indexOf('token') !== -1) {
       secret = splitingCookies[i].substring(splitingCookies[i].indexOf('=') + 1);
-      sendToken();
+      validateToken();
       break;
+    } else if (splitingCookies.length - 1 === i) {
+      redirectToSignIn();
     }
   }
 
-  function sendToken() {
+  function validateToken() {
     axios.post('http://localhost:9001/isAuthorized', {
       token: secret
     })
-    .then(() => {
-      next();
+    .then(response => {
+      callback({type: 'SIGNUP_SUCCESS', auth: response.data });
+      browserHistory.push('/');
     })
     .catch(() => {
-      replace('/signin');
-      next();
+      browserHistory.push('/signin');
     });
   }
+
+  function redirectToSignIn() {
+    browserHistory.push('/signin');
+  }
 };
-
-
