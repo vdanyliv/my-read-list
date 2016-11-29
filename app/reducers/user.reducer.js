755862fa9bfe520f18eb18b8536c4b3f330d5d4b
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-11-29 20:08:40 +0200

    Return new User object without extending with previous state

--- app/reducers/user.reducer.js
+++ app/reducers/user.reducer.js
@@ -14,7 +14,7 @@ export default function(state = initialState, action) {
   switch (action.type) {
   case 'SIGNUP_SUCCESS':
   case 'SIGNIN_SUCCESS':
-    return Object.assign({}, state, {
+    return Object.assign({}, {
       user: action.auth.user,
       authorized: action.auth.authorized,
       secretToken: action.auth.secretToken,
@@ -22,7 +22,7 @@ export default function(state = initialState, action) {
     });
   case 'SIGNUP_FAILURE':
   case 'SIGNIN_FAILURE':
-    return Object.assign({}, state, {
+    return Object.assign({}, {
       error: action.auth.error,
       authorized: false
     });
