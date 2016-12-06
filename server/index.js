69a9f2c0288469be3118b7a2ea3ab24ab384ea08
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-12-06 16:22:57 +0200

    connect new api with application

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
-module.exports = function(app, passport) {
-  require('./api/auth-api')(app, passport);
+module.exports = function(app) {
+  require('./api/auth-api')(app);
+  require('./api/my-books-api')(app);
 };
